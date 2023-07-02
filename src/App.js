63636bc60edcc27b07a5506871ad406089a847cd
VadimKorobec <_s_e_t__@ukr.net>
2023-07-02 17:23:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import { Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Products } from "./pages/Products";
 import { NotFound } from "./pages/NotFound";
+import { Mission } from "./pages/Mission";
+import { Team } from "./pages/Team";
+import { Reviews } from "./pages/Reviews";
+import styled from "styled-components";
+import { ProductDetails } from "./pages/ProductDetails";
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 export const App = () => {
   return (
     <>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
