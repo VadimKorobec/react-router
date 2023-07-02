7981dd1da194c0eacb540bf33442fee0f7c7e995
VadimKorobec <_s_e_t__@ukr.net>
2023-07-02 17:55:19 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,21 @@
+import { Link, Outlet } from "react-router-dom";
+
 export const About = () => {
-  return <>About</>;
+  return (
+    <>
+      <h1>About page</h1>
+      <ul>
+        <li>
+          <Link to="mission">Mission</Link>
+        </li>
+        <li>
+          <Link to="team">Team</Link>
+        </li>
+        <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </>
+  );
 };
