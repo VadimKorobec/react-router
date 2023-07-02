9a3a82809b8a20d518288c143bdaa53dadf67062
VadimKorobec <_s_e_t__@ukr.net>
2023-07-02 16:44:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Products } from "./pages/Products";
+import { NotFound } from "./pages/NotFound";
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 };
