258cd21acab5d295adec74ab2c9f5bb574ed2b7c
VadimKorobec <_s_e_t__@ukr.net>
2023-07-06 15:11:29 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,5 +1,5 @@
-import { StatusFilter } from "components/StatusFilter/StatusFilter";
-import { TaskCounter } from "components/TaskCounter/TaskCounter";
+import { StatusFilter } from "../StatusFilter/StatusFilter";
+import { TaskCounter } from "../TaskCounter/TaskCounter";
 import css from "./AppBar.module.css";
 
 export const AppBar = () => {
