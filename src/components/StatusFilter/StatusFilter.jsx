933ab940ca9195716a60149a1849cb492713b875
VadimKorobec <_s_e_t__@ukr.net>
2023-07-06 16:42:56 +0300

    Update StatusFilter.jsx

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux";
+import { statusFilter } from "../../redux/constants";
 import { Button } from "../Button/Button";
 import css from "./StatusFilter.module.css";
+import { getStatusFilter } from "../../redux/selectors";
 
 export const StatusFilter = () => {
+  const filter = useSelector(getStatusFilter);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilter.all}>All</Button>
+      <Button selected={filter === statusFilter.active}>Active</Button>
+      <Button selected={filter === statusFilter.completed}>Completed</Button>
     </div>
   );
 };
