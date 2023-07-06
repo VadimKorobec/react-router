a8fc981f3e35d8726608cf033bc1e15ac9aad851
VadimKorobec <_s_e_t__@ukr.net>
2023-07-06 14:40:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
-import { Products } from "./pages/Products";
-import { NotFound } from "./pages/NotFound";
-import { Mission } from "./pages/Mission";
-import { Team } from "./pages/Team";
-import { Reviews } from "./pages/Reviews";
-import { ProductDetails } from "./pages/ProductDetails";
-import { SharedLayout } from "./components/SharedLayout/SharedLayout";
-
 export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />}>
-            <Route path="mission" element={<Mission />} />
-            <Route path="team" element={<Team />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:productId" element={<ProductDetails />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <></>;
 };
